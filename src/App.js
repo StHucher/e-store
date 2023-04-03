fd4a619172e2b9ada5396a9e596a5fa428bfae99
Stephane <stephane.hucher40@gmail.com>
2023-04-03 10:00:59 +0200

    Display products by category

--- src/App.js
+++ src/App.js
@@ -6,25 +6,35 @@ import Category from './components/Category';
 
 function App() {
 
-  const [results, setResults] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
 
  React.useEffect(() => {
     fetch("http://localhost:3001/categories")
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      setResults(data)
+      setCategories(data)
     })
   }, [])
 
   const handleCategoryClick = (id) => {
-    alert('id:' + id);
+    fetch("http://localhost:3001/products?catId=" +id)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setProducts(data)
+    })
   }
 
   const renderCategories = () => {
-    return results.map(c => 
+    return categories.map(c => 
       <Category key={c.id} id={c.id} title={c.title} onCategoryClick={() => handleCategoryClick(c.id)} />
       );
+  }
+
+  const renderProducts = () => {
+    return products.map(p => <div>{p.title}</div> )
   }
 
   return (
@@ -35,12 +45,13 @@ function App() {
       <section>
         <nav>
           {
-            results &&  renderCategories()
+            categories &&  renderCategories()
           }
 
         </nav>
         <article>
-          main area
+          <h1>Products</h1>
+          { products && renderProducts() }
         </article>
         
       </section>
