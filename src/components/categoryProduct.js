3289466a2fc53d0682eefb7a1f07e553413ce39c
Stephane <stephane.hucher40@gmail.com>
2023-04-19 15:54:16 +0200

    Use styled component

--- src/components/categoryProduct.js
+++ src/components/categoryProduct.js
@@ -1,63 +1,140 @@
 
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+
 
 const CategoryProduct = ({id, title, image, specs, features, price, stock}) => {
     const navigate = useNavigate();
 
     return (
         <article>
-                <div className="category-product-title">
+                <ProductTitle>
                     <Link to={`products/${id}`}>{title}</Link>
-                </div>
+                </ProductTitle>
 
              <figure>
-                <div className="category-product-image-container">
-                    <img src={`./assets/${image}`} alt={title}/>
-                </div>
+                <ProductImageContainer>
+                    <ProductImageContainerImage src={`./assets/${image}`} alt={title}/>
+                </ProductImageContainer>
              </figure>
 
              <aside>
-                <div className="category-product-info-dimensions">
-                    <h3>Dimensions</h3>
+                <ProductInfo>
+                    <ProductInfoHeader>Dimensions</ProductInfoHeader>
                     <label>{specs.dimensions}</label>
-                </div>
+                </ProductInfo>
 
                 {specs.capacity &&
-                <div className="category-product-info-capacity">
-                    <h3>Capacity</h3>
+                <ProductInfo>
+                    <ProductInfoHeader>Capacity</ProductInfoHeader>
                     <label>{specs.capacity}</label>
-                </div>
+                </ProductInfo>
                 }
 
-                <div className="category-product-info-features">
-                    <h3>Features</h3>
+                <ProductInfo>
+                    <ProductInfoHeader>Features</ProductInfoHeader>
                     <ul>
                         {features?.map((f, i) => {
-                            return <li key={`feature${i}`}>{f}</li>
+                            return <ProductInfoListItem key={`feature${i}`}>{f}</ProductInfoListItem>
                         })}
                     </ul>
-                </div>
+                </ProductInfo>
              </aside>
 
              <aside className="category-product-finance">
-                <div className="category-product-finance-price">
+                <ProductInfoFinancePrice>
                     &euro;{price}
-                </div>
+                </ProductInfoFinancePrice>
 
-                <div className="category-product-info-stock">
-                    <label>Stock Level: {stock}</label>
-                    <label>FREE Delivery</label>
-                </div>
+                <ProductInfoStock>
+                    <ProductInfoStockLabel>Stock Level: {stock}</ProductInfoStockLabel>
+                    <ProductInfoStockLabel>FREE Delivery</ProductInfoStockLabel>
+                </ProductInfoStock>
 
-                <div className="category-product-action">
-                    <button onClick={() => navigate(`products/${id}`)}>View Product</button>
-                    <button>Add to Basket</button>
-                </div>                
+                <ProductInfoAction>
+                    <ProductInfoActionButton onClick={() => navigate(`products/${id}`)}>View Product</ProductInfoActionButton>
+                    <ProductInfoActionButton>Add to Basket</ProductInfoActionButton>
+                </ProductInfoAction>                
              </aside>
         </article>
        
     )
     
 }
-export default CategoryProduct
+export default CategoryProduct;
+
+const ProductTitle = styled.div`
+  grid-column: 1 / span 3;
+  color: darkslategray;
+  font-weight: bold;
+  font-size: 1.5em;
+  padding-left: 10px;
+`;
+
+const ProductImageContainer = styled.div`
+  padding:10px;
+  width: 60%;
+`;
+
+const ProductImageContainerImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductInfoHeader = styled.h3`
+    color: darkslategray;
+    font-size: 1em;
+    font-weight: bold;
+    padding-top: 10px;
+    padding-bottom: 5px;
+`;
+
+const ProductInfoListItem = styled.li`
+    padding-top: 5px;
+`;
+
+const ProductInfoStock = styled.div`
+  padding-left:10px;
+  margin-top: 20px;
+  padding-top: 10px;
+  background-color: lightgrey;
+  height: 20%;
+  width: 30%;
+  border-radius: 5px;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductInfoFinancePrice = styled.div`
+    color: darkslategray;
+    font-size: 2em;
+    font-weight: bold;
+    padding-top: 10px;
+`;
+
+const ProductInfoStockLabel = styled.label`
+    padding-bottom: 5px;
+`;
+
+const ProductInfoAction = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const ProductInfoActionButton = styled.button`
+    width: 160px;
+    height: 30px;
+    border-radius: 10px;
+    margin-top: 20px;
+    background-color: lightgray;
+    border: solid 1px slategrey;
+    font-weight: bold;
+`;
--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 
 import { getProductById } from "../fetcher";
+import styled from "styled-components";
 
 const ProductDetail = () => {
     const [product, setProduct] = useState({errorMessage: '', data: {} });
@@ -18,59 +19,137 @@ const ProductDetail = () => {
 
     return (
         <article>
-                <div className="category-product-title">
+                <ProductTitle>
                     {product.data.title}
-                </div>
+                </ProductTitle>
 
              <figure>
-                <div className="category-product-image-container">
-                    <img src={`/assets/${product.data.image}`} alt={product.data.title}/>
-                </div>
+                <ProductImageContainer>
+                    <ProductImageContainerImage src={`/assets/${product.data.image}`} alt={product.data.title}/>
+                </ProductImageContainer>
              </figure>
 
              <aside>
-                <div className="category-product-info-dimensions">
-                    <h3>Dimensions</h3>
+                <ProductInfo>
+                    <ProductInfoHeader>Dimensions</ProductInfoHeader>
                     {<label>{product.data.specs?.dimensions}</label> }
-                </div>
+                </ProductInfo>
 
                 { product.data.specs?.capacity &&
-                <div className="category-product-info-capacity">
-                    <h3>Capacity</h3>
+                <ProductInfo>
+                    <ProductInfoHeader>Capacity</ProductInfoHeader>
                     <label>{product.data.specs?.capacity}</label>
-                </div>
+                </ProductInfo>
                  }
 
-                <div className="category-product-info-features">
-                    <h3>Features</h3>
+                <ProductInfo>
+                    <ProductInfoHeader>Features</ProductInfoHeader>
                     <ul>
                         {product.data.features?.map((f, i) => {
-                            return <li key={`feature${i}`}>{f}</li>
+                            return <ProductInfoListItem key={`feature${i}`}>{f}</ProductInfoListItem>
                         })}
                     </ul>
-                </div>
+                </ProductInfo>
              </aside>
 
              <aside className="category-product-finance">
-                <div className="category-product-finance-price">
+                <ProductInfoFinancePrice>
                     &euro;{product.data.price}
-                </div>
+                </ProductInfoFinancePrice>
 
-                <div className="category-product-info-stock">
-                    <label>Stock Level: {product.data.stock}</label>
-                    <label>FREE Delivery</label>
-                </div>
+                <ProductInfoStock>
+                    <ProductInfoStockLabel>Stock Level: {product.data.stock}</ProductInfoStockLabel>
+                    <ProductInfoStockLabel>FREE Delivery</ProductInfoStockLabel>
+                </ProductInfoStock>
 
-                <div className="category-product-action">
-                    <button>Add to Basket</button>
-                </div>                
+                <ProductInfoAction>
+                    <ProductInfoActionButton>Add to Basket</ProductInfoActionButton>
+                </ProductInfoAction>                
              </aside>
 
-            <div>{product.data?.description}</div>
+            <ProductDescription>{product.data?.description}</ProductDescription>
 
         </article>
     )
     
 }
 
-export default ProductDetail
+export default ProductDetail;
+
+const ProductDescription = styled.div`
+    grid-column: 1 / span 3;
+`;
+
+const ProductTitle = styled.div`
+  grid-column: 1 / span 3;
+  color: darkslategray;
+  font-weight: bold;
+  font-size: 1.5em;
+  padding-left: 10px;
+`;
+
+const ProductImageContainer = styled.div`
+  padding:10px;
+  width: 60%;
+`;
+
+const ProductImageContainerImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductInfoHeader = styled.h3`
+    color: darkslategray;
+    font-size: 1em;
+    font-weight: bold;
+    padding-top: 10px;
+    padding-bottom: 5px;
+`;
+
+const ProductInfoListItem = styled.li`
+    padding-top: 5px;
+`;
+
+const ProductInfoStock = styled.div`
+  padding-left:10px;
+  margin-top: 20px;
+  padding-top: 10px;
+  background-color: lightgrey;
+  height: 20%;
+  width: 30%;
+  border-radius: 5px;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductInfoFinancePrice = styled.div`
+    color: darkslategray;
+    font-size: 2em;
+    font-weight: bold;
+    padding-top: 10px;
+`;
+
+const ProductInfoStockLabel = styled.label`
+    padding-bottom: 5px;
+`;
+
+const ProductInfoAction = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const ProductInfoActionButton = styled.button`
+    width: 160px;
+    height: 30px;
+    border-radius: 10px;
+    margin-top: 20px;
+    background-color: lightgray;
+    border: solid 1px slategrey;
+    font-weight: bold;
+`;
